dc81e1c4f06f0e340cebac5c24c3e36b561b5cad
Mathieu Longe <mathieu.longe@orange.fr>
2020-12-18 15:03:06 +1100

    done. needs refactoring

---
+++ lib/bottles.rb
@@ -0,0 +1,29 @@
+class Bottles
+  def initialize
+  end
+
+  def song
+    verses(99, 0)
+  end
+
+  def verses(num_start, num_end)
+    result = ''
+    num_start.downto(num_end).to_a.each do |num|
+      result += verse(num) + "\n"
+    end
+    result.delete_suffix("\n")
+  end
+
+  def verse(num)
+    case num
+    when 2
+      "#{num} bottles of beer on the wall, #{num} bottles of beer.\nTake one down and pass it around, #{num - 1} bottle of beer on the wall.\n"
+    when 1
+      "#{num} bottle of beer on the wall, #{num} bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+    when 0
+      "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+    else
+      "#{num} bottles of beer on the wall, #{num} bottles of beer.\nTake one down and pass it around, #{num - 1} bottles of beer on the wall.\n"
+    end
+  end
+end
