968a65084046d3a67b54ee3ae4f3c75c177b64b7
Mathieu Longe <mathieu.longe@orange.fr>
2021-04-24 15:16:32 +1000

    done

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -13,13 +13,25 @@ class Bottles
   def verse(num)
     case num
     when 2
-      "#{num} bottles of beer on the wall, #{num} bottles of beer.\nTake one down and pass it around, #{num - 1} bottle of beer on the wall.\n"
+      "#{num} bottles of beer on the wall, " \
+      "#{num} bottles of beer.\n" \
+      'Take one down and pass it around, ' \
+      "#{num - 1} bottle of beer on the wall.\n"
     when 1
-      "#{num} bottle of beer on the wall, #{num} bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+      "#{num} bottle of beer on the wall, " \
+      "#{num} bottle of beer.\n" \
+      'Take it down and pass it around, ' \
+      "no more bottles of beer on the wall.\n"
     when 0
-      "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+      'No more bottles of beer on the wall, ' \
+      "no more bottles of beer.\n" \
+      'Go to the store and buy some more, ' \
+      "99 bottles of beer on the wall.\n"
     else
-      "#{num} bottles of beer on the wall, #{num} bottles of beer.\nTake one down and pass it around, #{num - 1} bottles of beer on the wall.\n"
+      "#{num} bottles of beer on the wall," \
+      " #{num} bottles of beer.\n" \
+      'Take one down and pass it around, ' \
+      "#{num - 1} bottles of beer on the wall.\n"
     end
   end
 end
