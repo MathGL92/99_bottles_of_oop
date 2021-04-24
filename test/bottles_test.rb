52c4b9544c1f6d8a01d61786be93b662a8c84e4b
Mathieu Longe <mathieu.longe@orange.fr>
2021-04-24 18:31:17 +1000

    starting chapter 5

--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -20,6 +20,24 @@ VERSE
     assert_equal expected, ::Bottles.new.verse(89)
   end
 
+#   def test_verse_7
+#     # skip
+#     expected = <<-VERSE
+# 7 bottles of beer on the wall, 7 bottles of beer.
+# Take one down and pass it around, one six-pack of beer on the wall.
+#     VERSE
+#     assert_equal expected, ::Bottles.new.verse(7)
+#   end
+
+#   def test_verse_6
+#     # skip
+#     expected = <<-VERSE
+# One six-pack of beer on the wall, one six-pack of beer.
+# Take one down and pass it around, 5 bottles of beer on the wall.
+#     VERSE
+#     assert_equal expected, ::Bottles.new.verse(6)
+#   end
+
   def test_verse_2
     # skip
     expected = <<-VERSE
